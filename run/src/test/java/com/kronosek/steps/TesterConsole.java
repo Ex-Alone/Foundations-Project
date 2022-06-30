d88b710286978065ab0a56c95902ea76c576f878
WinnerTakesAll <kronomail@protonmail.com>
2022-06-30 13:04:43 -0400

    second commit

--- run/src/test/java/com/kronosek/steps/TesterConsole.java
+++ run/src/test/java/com/kronosek/steps/TesterConsole.java
@@ -15,34 +15,34 @@ public class TesterConsole {
     
     WebDriver driver;
 
-    @Given   ("the tester is on the tester console")
-    public void the_tester_is_on_the_tester_console(){
+    @Given   ("the tester is logged into the console")
+    public void the_tester_is_logged_into_the_console (){
         TestRunner.driver.get("C:/Users/18623/Desktop/Project One/Automation/run/src/test/resources/webpages/TesterConsole.html");
     }
 
-    @When    ("the tester selects the defect id")
-    public void the_tester_selects_the_defect_id () {
-        By id = By.id("defect");
+    @When    ("the tester selects the defect id by level of priority")
+    public void the_tester_selects_the_defect_id_by_level_of_priority () {
+        By id = By.id("defectId");
         WebElement defect = TestRunner.driver.findElement(id);
         defect.sendKeys("44756");
     }
 
-    @When    ("the tester updates defect status to approved declined reject fixed or shelved")
-    public void the_tester_updates_defect_status_to_approved_or_declined () {
-        WebElement selectElement = driver.findElement(By.id("terms"));
-        Select selectObject = new Select(selectElement);
-        selectObject.selectByIndex(0);
+    @When    ("the tester updates defect status to approved declined rejected fixed or shelved")
+    public void the_tester_updates_defect_status_to_approved_declined_rejected_fixed_or_shelved () {
+        WebElement select = driver.findElement(By.id("condition"));
+        Select options = new Select(select);
+        options.selectByIndex(0);
     }
 
     @When    ("the tester clicks on button to apply changes")
     public void the_tester_clicks_on_button_to_apply_changes () {
-        By apply = By.id("button");
+        By apply = By.id("update");
         WebElement button = TestRunner.driver.findElement(apply);
         button.click();
     }
 
-    @Then    ("the tester should be notified of the changes via alert box")
-    public void the_tester_should_be_notified_of_the_changes_via_alert_box () {
+    @Then    ("the tester should be notified of the changes via an alert box")
+    public void the_tester_should_be_notified_of_the_changes_via_an_alert_box () {
         
     }
 
--- run/src/test/resources/features/TesterConsole.feature
+++ run/src/test/resources/features/TesterConsole.feature
@@ -2,12 +2,8 @@ Feature: Testers should be able to administer and monitor network security explo
 
     Scenario: As a tester I want to address this problem in linear progression
 
-        Given   the tester is on the tester console
-        When    the tester should enter their id
-        When    the tester clicks on the get button
-        When    the tester selects the defect id 
-        When    the tester updates status to approved or declined
+        Given   the tester is logged into the console
+        When    the tester selects the defect id by level of priority
+        When    the tester updates defect status to approved declined rejected fixed or shelved
         When    the tester clicks on button to apply changes
-        When    the tester updates status to rejected fixed or shelved
-        When    the tester clicks on button to update the status on defects
-        Then    the tester should be able to monitor the whole process from start to finish
+        Then    the tester should be notified of the changes an via alert box
